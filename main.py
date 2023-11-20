92b18964a80f4c9ccf4a44856669c344f9f3364d
ikcduarte1 <145998151+ikcduarte1@users.noreply.github.com>
2023-11-20 13:17:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,15 +4,20 @@ import os
 import sys
 import curses
 import random
-import entities
+import additional
+
 
 def prompt():
-    print("#######################################")
+    print("---------------------------------------")
     print(" HEY! WOULD YOU LIKE TO PLAY OUR GAME? ")
-    print("#######################################")
+    print("---------------------------------------")
     print("        input 'play' or 'exit'         ") 
-    print("#######################################")
-    print("      Created by CC2-1B-GROUP LATE     ")
+    print("---------------------------------------")
+    print("      Created by CC2B - GROUP LATE     ")
+    print("               MEMBERS:                ")
+    print("               DUARTE                  ")
+    print("               HABON                   ")
+    print("               PADILLA                 ")
     option = ""
     while option not in ['play','exit']: 
         option = input("> ").lower()
@@ -23,11 +28,10 @@ def prompt():
         else:
             print("Please input valid command('start' or 'quit')")
 def introduction():
-    print("################################################")
+    print("-----------------------------------------------")
     print("THANK YOU FOR CHOOSING TO PLAY OUR GAME, ENJOY!")
-    print("################################################")
-    input("input any key to continue... \n")
-
+    print("-----------------------------------------------")
+    additional.press_any_key_to_continue
     os.system('cls')
  
     introduction = """Welcome to 'Tales of the Pure: The Sanctified Skirmish' a thrilling adventure game that takes you on a journey
@@ -38,27 +42,22 @@ As the protagonist, you must navigate through polluted landscapes, engage in cha
 adversaries, and collect treasures, all while armed with an arsenal of unique cleaning tools and gadgets. 
 Your quest is to vanquish the vile forces of dirt and grime, and cleanse the world from their corruption and filth."""
 
-    print_text_slowly(introduction)
-    character_name = input(print_text_slowly("What is your characters name:"))
-    print_text_slowly(f"Welcome {character_name}!, your quest begins now!")
-    input("input any key to continue... \n")
+    additional.print_text_slowly(introduction)
+    additional.press_any_key_to_continue()
+    os.system('cls')
+    character_name = input(additional.print_text_slowly("What is your characters name:"))
+    additional.print_text_slowly(f"Welcome {character_name}!, your quest begins now!")
+    additional.press_any_key_to_continue()
     os.system('cls')
 
-    instructions = f"""
-Your goal is to find all 3 hidden treasures randomly placed around the maze, when you find these treasures you are then able
-to open a portal that brings you to Pollutiax's lair, there you can defeat him and free the world of his corruption and filth.
-BEWARE! the maze is filled with dirty monsters that can easily defile you if you aren't careful! GOOD LUCK! {character_name}"""
-    print_text_slowly(instructions)
-    input("input any key to continue... \n")
+    instructions = f"""Your goal is to find all 3 hidden treasures randomly placed around the maze, when you find these 
+    treasures you are then ableto open a portal that brings you to Pollutiax's lair, there you can defeat him and free 
+    the world of his corruption and filth. BEWARE! the maze is filled with dirty monsters that can easily defile you 
+    if you aren't careful! GOOD LUCK! {character_name}"""
+    additional.print_text_slowly(instructions)
+    additional.press_any_key_to_continue()
     os.system('cls')
 
-
-def print_text_slowly(text, delay=0.03):
-    for char in text:
-        print(char, end='', flush=True)
-        time.sleep(delay)
-    print()
-    return "> "
 
 
 
@@ -108,9 +107,28 @@ def move_player(player_pos, key):
     else:
         return player_pos
 
+def encounter_enemy():
+    from entities import Enemy
+    Pollutant = Enemy("Pollutant", 5, 20)
+    Toxigore = Enemy("Toxigore", 10, 15)
+    Contaminoid = Enemy("Contaminoid", 100, 50)
+    Pollutiax = Enemy("Pollutiax", 20, 500)
+    chance = random.randint(1, 100)
+    if chance <= 60:
+        pass
+    elif chance <= 85:
+        return Pollutant
+    elif chance <= 95:
+        return Toxigore
+    elif chance <= 100:
+        return Contaminoid
+
+def display_status(stdscr, status):
+    stdscr.addstr(curses.LINES - 18, 0, status, curses.A_REVERSE)
+    stdscr.refresh()
+
 def main(stdscr):
-    
-    curses.curs_set(0)  
+    curses.curs_set(0)
     stdscr.clear()
 
     rows = 11
@@ -120,19 +138,33 @@ def main(stdscr):
 
     while True:
         stdscr.clear()
+        display_status(stdscr, "Move with arrow keys. Press Esc to exit.")
+        stdscr.addstr(curses.LINES - 16, 4,f"Player moved to position: {player_pos}",curses.A_BOLD)
+        stdscr.addstr(curses.LINES - 14, 17,"STATUS",curses.A_BOLD)
+        stdscr.addstr(curses.LINES - 13, 12,"---------------")
+        stdscr.addstr(curses.LINES - 12, 13,"HP: 100")
+        stdscr.addstr(curses.LINES - 11, 13,"WEAPON: BROOM")
+        stdscr.addstr(curses.LINES - 10, 13,"INVENTORY: []")
+        stdscr.addstr(curses.LINES - 9, 12,"---------------")
+        stdscr.addstr(curses.LINES - 7, 16,"GAMEPLAY",curses.A_BOLD)
+        stdscr.addstr(curses.LINES - 6, 12,"---------------")
+        stdscr.addstr(curses.LINES - 5, 6, f"{encounter_enemy()}")
+        
         print_maze(stdscr, maze, player_pos)
-        stdscr.refresh()
+        
+        
 
         key = stdscr.getch()
 
-        if key == 27:  # 27 is the ASCII code for the Esc key
+        if key == 27:
             break
 
         new_pos = move_player(player_pos, key)
         if 0 <= new_pos[0] < rows and 0 <= new_pos[1] < cols and maze[new_pos[0]][new_pos[1]] != '#':
             player_pos = new_pos
-
+            
+            stdscr.refresh()
+            
 if __name__ == "__main__":
     prompt()
     curses.wrapper(main)
-
