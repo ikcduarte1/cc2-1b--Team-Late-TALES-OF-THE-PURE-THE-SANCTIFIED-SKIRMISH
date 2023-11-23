ccc5da6f621d967b62a0caeebd1c9d29878018d8
ikcduarte1 <145998151+ikcduarte1@users.noreply.github.com>
2023-11-23 18:19:44 +0800

    Update entities.py

--- entities.py
+++ entities.py
@@ -1,86 +1,76 @@
 import random
 
 
-class Weapons:
-    def __init__(self, name, damage):
-        self.name = name
+class Player:
+    def __init__(self, health, stamina, damage, hp_pots, enrg_pots):
+        self.health = health
+        self.stamina = stamina
         self.damage = damage
-    def attack(self):
-        print(f"Your character used {self.name} to attack and dealth {self.damage} damage!")
-    class CurrentWeapon:
-        def __init__(self, current_weapon):
-            self.current_weapon = current_weapon
-        def replace(self, new_weapon):
-            self.current_weapon = new_weapon
-            print(f"Replaced current weapon to {new_weapon.name}")
+        self.hp_pots = hp_pots
+        self.enrg_pots = enrg_pots
 
+    def attack(self, enemy):
+        damage = random.randint(1, 10)
+        enemy.health -= damage
+        return f"You dealt {damage} damage to your enemy"
 
-Broom = Weapons("Broom", random.randint(5,10))
-WaterGun = Weapons("Water Gun", random.randint(10,20))
-Vaccum = Weapons("Vaccum", random.randint(30,50))
-BleachBane = Weapons("Bleach Bane", random.randint(50, 100))
+    def use_hp_pot(self):
+        if self.hp_pots > 0:
+            self.health += random.randint(20, 50)
+            self.hp_pots -= 1
+            return "You used a potion and restored some health."
+        else:
+            return "You don't have any health potions left."
+        
+    def use_enrg_pot(self):
+        if self.enrg_pots > 0:
+            self.stamina += random.randint(20, 30)
+            self.enrg_pots -= 1
+            return "You used a potion and restored some energy."
+        else:
+            return "You don't have any energy potions left."
+
+    def run(self):
+        self.stamina -= 10
+        return "You ran away and lost 10 stamina."
 
 class Enemy:
     def __init__(self, name, damage, health):
         self.name = name
-        self.damage = damage
         self.health = health
-    
-    def attack(self):
-        print(f"The {self.name} attacks you for {self.damage} damage")
+        self.damage = damage
 
-    def attacked(self):
-        print(f"The current health of {self.name} is {self.health}")
-        if self.health <= 0:
-            print(f"The {self.name} has died")
-    
-    def __str__(self):
-        return f"You have encountered a {self.name}"
+    def attack(self, player):
+        damage = random.randint(1, self.damage)
+        player.health -= damage
+        return f"The enemy dealt {damage} damage to you"
     
 
-Pollutant = Enemy("Pollutant", 5, 20)
-Toxigore = Enemy("Toxigore", 10, 15)
-Contaminoid = Enemy("Contaminoid", 100, 50)
-Pollutiax = Enemy("Pollutiax", 20, 500)
+class Pots:
+    def __init__(self, type, power, count):
+        self.type = type
+        self.power = power
+        self.count = count
+
+class HealthPots(Pots):
+    ...
+
+
+class EnergyPots(Pots):
+    ...
+
+class Weapons:
+    def __init__(self, name, damage):
+        self.name = name
+        self.damage = damage
 
 
 class Treasures:
     def __init__(self, name):
         self.name = name
 
-safeguard_gun = Treasures("Safeguard Gun")
-shampoo_portal_juice = Treasures("Shampoo Portal Juice")
-shower_head = Treasures("Shower Head")
 
 
-
-class Character:
-    
-    alive = True
-    def __init__(self, health, collected_treasures, weapon):
-        self.health = health
-        self.collected_treasures = collected_treasures
-        self.weapon = weapon
-    
-    def heal(self):
-        pass
-        
-
-    def attacked(self):
-        print(f"Your current health is {self.health}")
-        if self.health <= 0:
-            print("You have DIED!")
-            alive = False
-
-
-myCharacter = Character(100, [], Broom)
-
-
-
-
-
-
-myCharacter = Character(100, [], Broom)
 
 
 
